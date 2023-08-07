3a40d4f0d9583074cd7ee6459c3149b716f74de4
Leandro Salustriano <leandro.salustriano@hotmail.com>
2023-08-07 07:59:56 -0300

    removed login-has-permissions case

--- src/http/routes/logins-has-permissions/logins-has-permissions.create.router.ts
+++
@@ -1,9 +0,0 @@
-import { Express } from "express";
-import { DelegatePermissionCreateController } from "src/http/controllers/logins-has-permissions/create/delegate-permission-create.controller";
-import { MiddlewareLoginsHasPermissionsDelegateDto } from "src/http/middleware/dto/logins-has-permissions/create/middleware-logins-has-permissions-delegate.dto";
-
-function LoginsHasPermissionsCreateRouter(app:Express) {
-  app.post("/login/permissions",MiddlewareLoginsHasPermissionsDelegateDto(),DelegatePermissionCreateController);
-}
-
-export default LoginsHasPermissionsCreateRouter;
